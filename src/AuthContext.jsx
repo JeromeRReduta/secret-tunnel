742f7372d841f7d4d940da7848e8d798ddd738cc
JRReduta <jrreduta@yahoo.com>
2025-07-16 17:07:37 -0700

    authorization working w/ mock data

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -5,19 +5,26 @@ const API = "https://fsa-jwt-practice.herokuapp.com";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [token, setToken] = useState();
-  const [location, setLocation] = useState("GATE");
+    const [token, setToken] = useState();
+    const [location, setLocation] = useState("GATE");
 
-  // TODO: signup
+    const signup = (username) => {
+        const mockData = { username: username, token: "Yuh" };
+        console.log("got mockData", mockData);
+        setToken(mockData.token);
+        setLocation("TABLET");
+    };
 
-  // TODO: authenticate
+    // TODO: authenticate
 
-  const value = { location };
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+    const value = { location, signup };
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
 }
 
 export function useAuth() {
-  const context = useContext(AuthContext);
-  if (!context) throw Error("useAuth must be used within an AuthProvider");
-  return context;
+    const context = useContext(AuthContext);
+    if (!context) throw Error("useAuth must be used within an AuthProvider");
+    return context;
 }
--- src/Entrance.jsx
+++ src/Entrance.jsx
@@ -1,27 +1,36 @@
+import { useAuth } from "./AuthContext";
+
 /** Users can enter their name to receive a token from the API. */
 export default function Entrance() {
-  // TODO: call signup when form is submitted
+    const { signup } = useAuth();
 
-  return (
-    <>
-      <h1>Cave Entrance</h1>
-      <p>Your journey has brought you to the base of a rocky mountain.</p>
-      <p>
-        The quickest path forward is through the mountain's winding tunnels, but
-        a sturdy metal gate sits closed before you.
-      </p>
-      <p>
-        Two giant badgers stand guard on either side of the gate, their eyes
-        fixed on you. The one on the left opens its mouth, and with a deep,
-        rumbling voice, it asks, "Who approaches? Speak your name."
-      </p>
-      <form>
-        <label>
-          Name
-          <input name="name" />
-        </label>
-        <button>Respond</button>
-      </form>
-    </>
-  );
+    function handleInputName(formData) {
+        console.log(`name is: ${formData.get("name")}`);
+        const name = formData.get("name");
+        signup(name);
+    }
+
+    return (
+        <>
+            <h1>Cave Entrance</h1>
+            <p>Your journey has brought you to the base of a rocky mountain.</p>
+            <p>
+                The quickest path forward is through the mountain's winding
+                tunnels, but a sturdy metal gate sits closed before you.
+            </p>
+            <p>
+                Two giant badgers stand guard on either side of the gate, their
+                eyes fixed on you. The one on the left opens its mouth, and with
+                a deep, rumbling voice, it asks, "Who approaches? Speak your
+                name."
+            </p>
+            <form action={handleInputName}>
+                <label>
+                    Name
+                    <input name="name" />
+                </label>
+                <button>Respond</button>
+            </form>
+        </>
+    );
 }
